1281e36ac1111f23a766ac3b3194a441cf2ff1ff
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2022-11-06 20:37:43 -0600

    add basic config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,10 @@
+require("mhcyr.plugins-setup")
+require("mhcyr.core.options")
+require("mhcyr.core.keymaps")
+require("mhcyr.core.colorscheme")
+require("mhcyr.plugins.nvim-tree")
+require("mhcyr.plugins.lualine")
+require("mhcyr.plugins.telescope")
+require("mhcyr.plugins.nvim-cmp")
+require("mhcyr.plugins.lsp.mason")
+
---
+++ .config/nvim/lua/mhcyr/plugins/lualine.lua
@@ -0,0 +1,8 @@
+-- import lualine plugin safely
+local status, lualine = pcall(require, "lualine")
+if not status then
+  return
+end
+
+lualine.setup()
+
